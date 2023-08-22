b8dde6ea0632cc2b4e8b74483faaefef7a60e837
jjaei <ieajnowk@gmail.com>
2023-08-22 18:38:52 +0900

    Enum 클래스 생성

---
+++ src/main/java/jpabook/domain/Address.java
@@ -0,0 +1,12 @@
+package jpabook.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
+@Getter
+@Embeddable
+public class Address {
+    private String city;
+    private String street;
+    private String zipcode;
+}
