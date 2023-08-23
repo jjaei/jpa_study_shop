5f7e0ac3ed6f1ed238a5c4f266e929f455615e8b
jjaei <ieajnowk@gmail.com>
2023-08-23 20:06:08 +0900

    롬복 사용해서 애노테이션 붙이기

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,7 +1,6 @@
 package jpabook.jpashop.repository;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -12,7 +11,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
     private final EntityManager em;
 
     public void save(Member member) {  // 회원 가입
