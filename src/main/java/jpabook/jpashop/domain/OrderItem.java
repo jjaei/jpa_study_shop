b7c2c4ca97c68b686148dfc3f11a224c4a930eba
jjaei <ieajnowk@gmail.com>
2023-08-24 16:54:41 +0900

    기본 생성자 추가

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -27,8 +27,12 @@ public class OrderItem {
 
     private int count; // 주문 수량
 
+    protected OrderItem() {
+
+    }
+
     // 생성 메서드
-    public static OrderItem orderItem(Item item, int orderPrice, int count) {
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
         OrderItem orderItem = new OrderItem();
         orderItem.setItem(item);
         orderItem.setOrderPrice(orderPrice);
