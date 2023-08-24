70ae9c250eafcb82aa7a994231229a850077187e
jjaei <ieajnowk@gmail.com>
2023-08-24 18:57:35 +0900

    주문 검색 기능 개발

---
+++ src/main/java/jpabook/jpashop/domain/OrderSearch.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderSearch {
+    private String memberName;  // 회원 이름
+    private String orderStatus;  // 주문 상태(ORDER, CANCEL)
+}
