f7254e4fd482db54557d89503a77b488a792a1a6
jjaei <ieajnowk@gmail.com>
2023-10-02 21:45:54 +0900

    Querydsl 샘플

--- src/main/java/jpabook/jpashop/domain/OrderSearch.java
+++ src/main/java/jpabook/jpashop/domain/OrderSearch.java
@@ -7,5 +7,5 @@ import lombok.Setter;
 @Setter
 public class OrderSearch {
     private String memberName;  // 회원 이름
-    private String orderStatus;  // 주문 상태(ORDER, CANCEL)
+    private OrderStatus orderStatus;  // 주문 상태(ORDER, CANCEL)
 }
--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
@@ -10,6 +12,9 @@ import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.QMember;
+import jpabook.jpashop.domain.QOrder;
 import jpabook.jpashop.repository.simplequery.OrderSimpleQueryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -17,6 +22,9 @@ import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static jpabook.jpashop.domain.QMember.*;
+import static jpabook.jpashop.domain.QOrder.*;
 
 @Repository
 @RequiredArgsConstructor
@@ -91,6 +99,36 @@ public class OrderRepository {
         cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
         TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
         return query.getResultList();
+    }
+
+    // Querydsl 적용
+    public List<Order> findAll(OrderSearch orderSearch) {
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        return query.select(order)
+                    .from(order)
+                    .join(order.member, member)
+                    .where(statusEq(orderSearch.getOrderStatus()),
+                            nameLike(orderSearch.getMemberName()))
+                    .limit(1000)
+                    .fetch();
+
+    }
+
+    private BooleanExpression nameLike(String memberName) {
+        if (!StringUtils.hasText(memberName)) {
+            return null;
+        }
+        return member.name.like(memberName);
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if (statusCond == null) {
+            return null;
+        }
+        return order.status.eq(statusCond);
     }
 
     public List<Order> findAllWithMemberDelivery() {
